fff7c983f87a7e9eceebaa27e1f7a4dba56dc176
longsophal-del <longsophal@gmail.com>
2026-09-16 19:27:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste the Google Apps Script Web App URL here after you deploy Code.gs
 // (Deploy > New deployment > Web app > copy the URL ending in /exec).
 // Until you do, the site runs on the bundled sample snapshot in data.js.
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbysEIKokh50lDSQe6v3M-3L0fhMEgw3SdrrsH3fcTQ/dev";
